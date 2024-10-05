b192cf141a2d784a550c312a8d44e11c451af375
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-05 22:58:50 +0530

    Updated Positions

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
-screen.bgcolor("balck")
+screen.bgcolor("black")
 screen.title("My Snake Game")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 segment1 = Turtle(shape = "square")
 segment1.color("white")
 
 segment2 = Turtle(shape = "square")
 segment2.color("white")
+segment2.goto(-20, 0)
 
 segment3 = Turtle(shape = "square")
 segment3.color("white")
+segment3.goto(-40, 0)
 
 
 
