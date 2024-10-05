e6d9eda550c367877c50b18c9a1ac8b4aba22862
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-05 22:45:54 +0530

    Created Snake Body

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ screen.setup(width = 600, height = 600)
 screen.bgcolor("balck")
 screen.title("My Snake Game")
 
+segment1 = Turtle(shape = "square")
+segment1.color("white")
+
+segment2 = Turtle(shape = "square")
+segment2.color("white")
+
+segment3 = Turtle(shape = "square")
+segment3.color("white")
+
+
+
+
+
+
 
 screen.exitonclick()
