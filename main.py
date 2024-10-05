1fe9c5f040dfaf3e1a2b9159c0d1bd7a553ea460
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-05 23:07:52 +0530

    Updated Positions

--- main.py
+++ main.py
@@ -8,17 +8,10 @@ screen.title("My Snake Game")
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segment1 = Turtle(shape = "square")
-segment1.color("white")
-
-segment2 = Turtle(shape = "square")
-segment2.color("white")
-segment2.goto(-20, 0)
-
-segment3 = Turtle(shape = "square")
-segment3.color("white")
-segment3.goto(-40, 0)
-
+for position in starting_positions:
+    new_segment = Turtle(shape = "square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
