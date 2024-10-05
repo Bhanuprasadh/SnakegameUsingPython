9dc5a51db4c4e85a211c3dee4d3145666091b907
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-05 22:41:30 +0530

    Created Snake Body

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as TK
+from turtle import Screen, Turtle 
+
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("balck")
+screen.title("My Snake Game")
+
+
+screen.exitonclick()
