0313f08860e1bc16985f2c481aa9095490428df6
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-06 09:25:11 +0530

    Updated Snake to move

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen = Screen()
 screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
+screen.tracer(0)
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 
@@ -17,10 +17,13 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
+
+
 game_is_on = True
 while game_is_on:
     for seg in segments:
         seg.forward(20)
+        screen.update()
 
 
 
