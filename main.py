c7bbb25fa2961319dd5390f809d44f2e86e6616c
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-31 02:49:15 +0530

    Complete Snake Game

--- main.py
+++ main.py
@@ -30,11 +30,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+        snake.extend()
 
 
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
             
 
